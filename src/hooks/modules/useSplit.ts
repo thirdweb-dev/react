2616cc27084bf41e69edbd4465cd45727cbc9f36
Jonas Daniels <jonas.daniels@outlook.com>
2022-03-02 17:03:24 -0800

    usesplit

--- src/hooks/modules/useSplit.ts
+++ src/hooks/modules/useSplit.ts
@@ -1,6 +1,6 @@
 import { Split } from "@thirdweb-dev/sdk";
 import { useContract } from "./useContract";
 
-export function useSplitModule(moduleAddress?: string): Split | undefined {
+export function useSplit(moduleAddress?: string): Split | undefined {
   return useContract("split", moduleAddress);
 }
