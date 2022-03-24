02e7c4ad64d56c9613325627cc743d75c592019d
Jonas Daniels <jonas.daniels@outlook.com>
2022-03-24 01:53:07 -0700

    update things

--- src/components/MediaRenderer.tsx
+++ src/components/MediaRenderer.tsx
@@ -442,6 +442,7 @@ export const MediaRenderer = React.forwardRef<
           >
             <AiOutlineFileUnknown style={{ height: "64px", width: "64px" }} />
             <a
+              rel="noopener noreferrer"
               style={{
                 display: "flex",
                 alignItems: "center",
