ccb0880d1e975aaa239a4a98b86cf277e908f741
Furqan Rydhan <furqan.rydhan@gmail.com>
2022-08-04 07:54:15 +0000

    basic action button

--- src/components/Web3Button.tsx
+++ src/components/Web3Button.tsx
@@ -4,28 +4,29 @@ import React, { useCallback } from "react";
 export interface Web3ButtonType {
   contractAddress: string;
   funcName: string;
-  args: any[];
+  params: any[];
+  props: string;
 }
 
 /**
- * @param Web3ButtonType - contractAddress, funcName, args
+ * @param Web3ButtonType - contractAddress, funcName, params
  * @returns button that calls smart contract function
  *
  * @example
  * Usage with a contract:
  * ```jsx
- * <Web3Button contractAddress="0x..." funcName="foo" args={[1, 2, 3]} />
+ * <Web3Button contractAddress="0x..." funcName="foo" params={[1, 2, 3]} />
  * ```
  */
 export const Web3Button: React.FC<Web3ButtonType> = (
-  { contractAddress, funcName, args },
+  { contractAddress, funcName, params },
   props,
 ) => {
   const { contract } = useContract(contractAddress);
 
   const buttonClick = useCallback(() => {
     if (contract) {
-      contract.call(funcName, ...args).then(console.log);
+      contract.call(funcName, ...params).then(console.log);
     }
   }, [contract]);
 
