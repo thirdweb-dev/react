9375ee9eb578f01b2eb4e562368f6d7187cb0f6f
adam-maj <mr.adam.maj@gmail.com>
2022-08-12 14:14:59 -0400

    Add error callback and useLogout hook

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,19 +1,36 @@
 import { useSDK } from "../../Provider";
 import { useThirdwebConfigContext } from "../../contexts/thirdweb-config";
 import { LoginOptions } from "@thirdweb-dev/sdk/dist/src/schema";
+import React from "react";
 import invariant from "tiny-invariant";
 
-export function useLogin() {
+export interface LoginConfig {
+  domain: string;
+  redirectTo: string;
+  onError: (error: string) => void;
+}
+
+export function useLogin({ domain, redirectTo, onError }: LoginConfig) {
   const sdk = useSDK();
   const { authUrl } = useThirdwebConfigContext();
 
-  async function login(domain: string, options: LoginOptions) {
+  React.useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const error = queryParams.get("error");
+
+    if (error) {
+      // If there is an error, parse it and trigger the onError callback
+      onError(decodeURI(error));
+    }
+  }, []);
+
+  async function login(options?: LoginOptions) {
     invariant(authUrl, "Please specify an authUrl in the ThirdwebProvider");
     const payload = await sdk?.auth.login(domain, options);
     const encodedPayload = encodeURIComponent(JSON.stringify(payload));
 
     // Redirect to the login URL with the encoded payload
-    window.location.href = `${authUrl}/login?payload=${encodedPayload}`;
+    window.location.href = `${authUrl}/login?payload=${encodedPayload}&redirectTo=${redirectTo}`;
   }
 
   return login;
--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -1,3 +1,13 @@
+import { useThirdwebConfigContext } from "../../contexts/thirdweb-config";
+import invariant from "tiny-invariant";
+
 export function useLogout() {
-  return;
+  const { authUrl } = useThirdwebConfigContext();
+
+  function logout() {
+    invariant(authUrl, "Please specify an authUrl in the ThirdwebProvider");
+    window.location.href = `${authUrl}/logout`;
+  }
+
+  return logout;
 }
