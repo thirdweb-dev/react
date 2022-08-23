b007666e03a6af336029dedb7aada54576595fb4
Jonas Daniels <jonas.daniels@outlook.com>
2022-08-23 14:59:41 -0700

    remove extra file

--- packages/thirdweb-react/src/hooks/auth/index.ts
+++ packages/thirdweb-react/src/hooks/auth/index.ts
@@ -1,4 +1,19 @@
+import { LoginConfig, useLogin } from "./useLogin";
+import { useLogout } from "./useLogout";
+import { useUser } from "./useUser";
+
 export * from "./useLogin";
 export * from "./useLogout";
 export * from "./useUser";
-export * from "./useAuth";
+
+/**
+ *
+ * @returns
+ * @internal
+ */
+export function useAuth(loginConfig?: LoginConfig) {
+  const user = useUser();
+  const login = useLogin(loginConfig);
+  const logout = useLogout();
+  return { ...user, login, logout };
+}
